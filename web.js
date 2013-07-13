8cee0b457bb7f8e4f95b842329999514af4b98ee
ellisgiles <ellis.r.giles@gmail.com>
2013-07-13 04:21:07 +0000

    removed i

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-	response.send(fs.readFileiSync('index.hml').toString());
+	response.send(fs.readFileSync('index.hml').toString());
 });
 
 var port = process.env.PORT || 5000;
